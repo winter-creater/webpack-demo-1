99124550b3c4bd76958934390f3051c99d248396
winter <1304879929@qq.com>
2021-07-22 16:41:10 +0800

    两个配置文件，并继承一个相同的内容的配置文件

---
+++ webpack.config.base.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+
+module.exports = {
+    // mode: 'development',
+    //入口文件
+    entry: './src/index.js',
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        //filename: 'index.js',//转译出口文件
+        filename: '[name].[contenthash].js'
+    },
+    // devtool: 'inline-source-map',
+    // devServer: {
+    //     contentBase: './dist',
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            // filename: 'assets/admin.html',//转译的文件目录
+            template: './src/assets/index.html'//本地文件路径
+        }),
+    ],
+
+    // module: {
+    //     rules: [
+    //     ],
+    // },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
+const base = require('./webpack.config.base.js')
 
 module.exports = {
+
     mode: 'development',
-    //入口文件
-    entry: './src/index.js',
-    output: {
-        // path: path.resolve(__dirname, 'dist'),
-        //filename: 'index.js',//转译出口文件
-        filename: '[name].[contenthash].js'
-    },
+    ...base,
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My App',
-            // filename: 'assets/admin.html',//转译的文件目录
-            template: './src/assets/index.html'//本地文件路径
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css',
-            ignoreOrder: false,
-        }),
-    ],
-
+    ...base,
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-                // use: [
-                //     {
-                //         loader: MiniCssExtractPlugin.loader,
-                //         options: {
-                //             publicPath: '../',
-                //             hmr: process.env.NODE_ENV === "development",
-                //         },
-                //     },
-                //     "css-loader"
-                // ],
+                use: ["style-loader", "css-loader"],
+
             },
         ],
     },
