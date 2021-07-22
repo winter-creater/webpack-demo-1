a25f779486c9ffcb3ad6c3daff33530d3bd633f2
winter <1304879929@qq.com>
2021-07-22 13:13:35 +0800

    webpack 自动生成html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',//入口文件
+    //入口文件
+    entry: './src/index.js',
     output: {
         // path: path.resolve(__dirname, 'dist'),
         //filename: 'index.js',//转译出口文件
         filename: '[name].[contenthash].js'
     },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        // filename: 'assets/admin.html',//转译的文件目录
+        template: './src/assets/index.html'//本地文件路径
+    })],
 };
