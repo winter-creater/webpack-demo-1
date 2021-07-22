2cb1c80db0cdb0f86d12cf60dabc71b5cfd390d2
winter <1304879929@qq.com>
2021-07-22 15:32:16 +0800

    把css抽成文件放在head标签MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 
 module.exports = {
@@ -15,16 +17,34 @@ module.exports = {
     devServer: {
         contentBase: './dist',
     },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'My App',
-        // filename: 'assets/admin.html',//转译的文件目录
-        template: './src/assets/index.html'//本地文件路径
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            // filename: 'assets/admin.html',//转译的文件目录
+            template: './src/assets/index.html'//本地文件路径
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+            ignoreOrder: false,
+        }),
+    ],
+
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                // use: [
+                //     {
+                //         loader: MiniCssExtractPlugin.loader,
+                //         options: {
+                //             publicPath: '../',
+                //             hmr: process.env.NODE_ENV === "development",
+                //         },
+                //     },
+                //     "css-loader"
+                // ],
             },
         ],
     },
